17582f76172f5baa4890d33d4327e2d878d16f78
Zeu Cxb <zeucxb@MacBook-Pro-de-Zeu.local>
2016-12-03 19:08:47 -0200

    This add a new module directory to make easier understand the project organization

--- src/router.js
+++ src/router.js
@@ -2,6 +2,6 @@
 
 const router = require('express').Router();
 
-router.use('/pokemons', require('./pokemon/routes'));
+router.use('/pokemons', require('./modules/pokemon/routes'));
 
 module.exports = router;
